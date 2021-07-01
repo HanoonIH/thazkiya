9820365a9fcdd8e76d785e9e545f453668d2ccdc
HanoonIH <hanoonih123@gmail.com>
2021-07-01 15:53:45 +0530

    styled with tailwind, tags, recommended posts, landing etc

--- .eleventy.js
+++ .eleventy.js
@@ -2,8 +2,18 @@ const { DateTime } = require('luxon');
 
 module.exports = config => {
 
+    // Passthrough
+    config.addPassthroughCopy('./src/assets/');
+    config.addPassthroughCopy('./src/style.css');
+
+    // Filters
     config.addFilter("postDate", (dateObj) => {
         return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+    });
+
+    // Returns a collection of blog posts in reverse date order
+    config.addCollection('post', collection => {
+        return [...collection.getFilteredByGlob('./src/posts/*.md')].reverse();
     });
 
     return {
