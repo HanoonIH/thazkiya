7bf2ad25805bfae1cb280883e9145112b8bfef14
HanoonIH <66325036+HanoonIH@users.noreply.github.com>
2021-08-08 12:48:47 +0530

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -20,7 +20,7 @@ module.exports = config => {
         // return [...collection.getFilteredByGlob('./src/posts/*.md').reverse()];
         // return [...collection.getFilteredByGlob('./src/posts/*.md')].sort(function(a, b){ return a.number - b.number });
         let posts = collection.getFilteredByGlob('./src/posts/*.md');
-        posts.sort(function(a, b){return a-b});
+        // posts.sort(function(a, b){return a-b});
         return posts;
     });
 
